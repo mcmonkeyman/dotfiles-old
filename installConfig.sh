84448cff166ea985ee84b9b414e85bceb484c789
Eoin O'Connor <eoconnor@reonomy.com>
2016-01-15 18:22:13 -0500

    Add brew install script

---
+++ installConfig.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+#
+# Brew packages that I use alot.
+#
+brew install git
+brew cask install java
+brew install scala --with-docs
+brew install sbt
+
+#
+# Reonomy Brew packages.
+#
+brew install postgres
+brew install postgis
+brew install graphviz
+brew install node
+brew install npm
+
+#
+# Install updated Vim
+#
+brew uninstall vim
+brew install luajit
+brew install vim --with-luajit --override-system-vim
+
+#
+# Some cask packages that I like.
+#
+brew cask install slack
+brew cask install alfred
+brew cask install amethyst
+brew cask install intellij-idea-ce
+brew cask install tunnelblick
+brew cask install jumpcut
+brew cask install spotify
+brew cask install sublime-text
+brew cask install seil
+brew cask install captur
+brew cask install sourcetree
+brew cask install caffeine
+brew cask install karabiner
+brew cask install pgadmin3
+
+#
+# Link alfred to cask
+#
+brew cask alfred link
+
+
+#
+# Install conscript
+#
+curl https://raw.githubusercontent.com/n8han/conscript/master/setup.sh | sh
+
