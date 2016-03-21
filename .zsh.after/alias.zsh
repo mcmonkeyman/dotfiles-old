af3a643f39d2d9200340c20f9a8681b4ee394721
Eoin O'Connor <eoconnor@reonomy.com>
2016-03-21 17:24:11 -0400

    Add bu alias

--- .zsh.after/alias.zsh
+++ .zsh.after/alias.zsh
@@ -2,3 +2,8 @@
 alias rwh='echo "${PWD##*/}" | xargs tmux rename-window'
 alias rw='tmux rename-window'
 alias sa='source ~/.zsh.after/*'
+
+
+# system aliases
+## backup the current dir to dir.backup
+alias bu='DIR=${PWD##*/}; rm -rf ../${DIR}.backup; cp -R ../${DIR} ../${DIR}.backup; ls ../ | grep ${DIR}'
