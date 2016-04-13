255c1fdb3149c8fe0409939d7a0bf0e28131ed11
Eoin O'Connor <eoconnor@reonomy.com>
2016-04-13 16:55:24 -0400

    Add tmux alias

--- .zsh.after/alias.zsh
+++ .zsh.after/alias.zsh
@@ -1,8 +1,8 @@
 # tmux aliases
 alias rwh='echo "${PWD##*/}" | xargs tmux rename-window'
 alias rw='tmux rename-window'
+alias kw='tmux kill-window'
 alias sa='source ~/.zsh.after/*'
-
 
 # system aliases
 ## backup the current dir to dir.backup
