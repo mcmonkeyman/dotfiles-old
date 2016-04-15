374d5c3c216afcf11b0313d57815251949c6622f
Eoin O'Connor <eoconnor@reonomy.com>
2016-04-15 16:21:16 -0400

    Add some usefull aliases

--- .zsh.after/alias.zsh
+++ .zsh.after/alias.zsh
@@ -1,9 +1,24 @@
+#
 # tmux aliases
+#
 alias rwh='echo "${PWD##*/}" | xargs tmux rename-window'
 alias rw='tmux rename-window'
 alias kw='tmux kill-window'
-alias sa='source ~/.zsh.after/*'
 
+#
+# alias aliases
+#
+alias sa='source ~/.zsh.after/*'
+alias ca='cat ~/.zsh.after/alias.zsh'
+alias ea='vim ~/.zsh.after/alias.zsh'
+
+#
 # system aliases
+#
 ## backup the current dir to dir.backup
 alias bu='DIR=${PWD##*/}; rm -rf ../${DIR}.backup; cp -R ../${DIR} ../${DIR}.backup; ls ../ | grep ${DIR}'
+
+#
+# fun aliases
+#
+alias weather='curl http://wttr.in/'
